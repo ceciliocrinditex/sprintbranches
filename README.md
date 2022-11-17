db0730e97e13cab6cb142afaa25690841224c8b6
ceciliocrinditex <98752687+ceciliocrinditex@users.noreply.github.com>
2022-11-17 09:43:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sprint Branches
+
+Generate (and remove) temporary branches of each Sprint.
+
+### Usage
+
+```sh
+$ npm i
+```
+
+Rename the file **config.example.json** to **config.json** and configure it.
+
+```sh
+$ node ./sprintBranches.js
+```
+
+Enjoy!
+
+### Requirements
+
+- Node >= 10
+
+### FAQ
+
+- How to generate a Github token ?
+https://docs.github.com/es/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
---
+++ sprintBranches.js
@@ -0,0 +1,134 @@
+const readline = require("readline");
+const config = require("./config.json");
+const { Octokit } = require("@octokit/core");
+const { clear } = require("node:console");
+
+const REQUIRED_NODE_VERSION = 10;
+
+async function checkRequirements() {
+  const nodeVersion = process.version.split("v")[1];
+  const majorNodeVersion = Number(nodeVersion.split(".")[0]);
+
+  if (majorNodeVersion < REQUIRED_NODE_VERSION) {
+    console.warn(`Using a node version lower than ${REQUIRED_NODE_VERSION} could cause problems\n`);
+  }
+
+  if (!config.repositories || !config.repositories.length > 0) {
+    console.error("You need to add the repositories");
+    process.exit(1);
+  }
+
+  if (!config.branches || !config.branches.length > 0) {
+    console.error("You need to add the branches");
+    process.exit(1);
+  }
+
+  if (!config.token) {
+    console.error("You need to add a Github token");
+    process.exit(1);
+  }
+
+  if (!config.branchFrom) {
+    console.error("You need to add a branch");
+    process.exit(1);
+  }
+
+  if (!config.owner) {
+    console.error("You need to add a owner");
+    process.exit(1);
+  }
+}
+
+function checkAnswer(answer) {
+  if (answer.toLowerCase().includes("y") || answer.toLowerCase().includes("yes")) {
+    return true;
+  }
+
+  return false;
+}
+
+async function init() {
+  clear();
+
+  console.time("Execution time");
+
+  await checkRequirements();
+
+  // const tokenBuffer = Buffer.from(config.token, "base64");
+  // const tokenDecoded = tokenBuffer.toString("utf-8");
+
+  const octokit = new Octokit({
+    auth: config.token,
+  });
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let deleteBranches = false;
+  const wantDeleteBranches = await new Promise((resolve) => {
+    rl.question("\nWant to delete existing branches? (Y or Yes) or (N): ", resolve);
+  });
+
+  if (checkAnswer(wantDeleteBranches)) {
+    const confirmDeleteBranches = await new Promise((resolve) => {
+      rl.question("Are you sure? (Y or Yes) or (N): ", resolve);
+    });
+
+    deleteBranches = checkAnswer(confirmDeleteBranches);
+  }
+
+  rl.close();
+
+  console.info("\nNumber of repositories:\x1b[1m", config.repositories.length, "\x1b[0m");
+
+  console.info("Number of branches:\x1b[1m", config.branches.length, "\x1b[0m");
+
+  console.info("\n-----------------------------\n");
+
+  for (const repository of config.repositories) {
+    const branches = await octokit.request(`GET /repos/{owner}/{repo}/git/refs/heads`, {
+      owner: config.owner,
+      repo: repository,
+    });
+
+    let sha;
+    let remoteBranchExists = false;
+    for (const branch of config.branches) {
+      for (const remoteBranch of branches.data) {
+        if (remoteBranch.ref.includes(branch)) {
+          if (deleteBranches) {
+            await octokit.request("DELETE /repos/{owner}/{repo}/git/refs/{ref}", {
+              owner: config.owner,
+              repo: repository,
+              ref: `heads/${branch}`,
+            });
+
+            continue;
+          }
+
+          console.warn(`"${branch}" already exists in ${repository}`);
+          remoteBranchExists = true;
+          continue;
+        }
+
+        if (remoteBranch.ref.includes(config.branchFrom)) sha = remoteBranch.object.sha;
+      }
+
+      if (remoteBranchExists) continue;
+
+      await octokit.request(`POST /repos/{owner}/{repo}/git/refs`, {
+        owner: config.owner,
+        repo: repository,
+        ref: `refs/heads/${branch}`,
+        sha,
+      });
+    }
+  }
+
+  console.info("\n\x1b[32m\x1b[1mDone!\x1b[0m\n");
+  console.timeEnd("Execution time");
+}
+
+init();
